ab41c13b8675c008e759df582061aa3e455950f8
ljbdelacruz123 <ljbdelacruz123@gmail.com>
2016-11-18 16:12:47 +0800

    added games data in for ps vita

--- services/data/GamesList.data.js
+++ services/data/GamesList.data.js
@@ -1,5 +1,7 @@
 "use strict";
-exports.psv = [{ id: 1, title: 'Alpha Saphire', link: [{ link: 'http://www.google.com' }], image: [{ src: '' }, { src: '' }], imageCover: "" }];
-exports.t3dsGames = [{ id: 1, title: 'Warrio Land', link: [{ link: 'www.facebook.com' }], image: [{ src: '' }], imageCover: "" },];
-exports.psp = [{ id: 1, title: 'Final Fantasy Tactics', link: [{ link: 'www.facebook.com' }], image: [{ src: '' }, { src: '' }], imageCover: "" },];
+exports.psv = [{ id: 1, title: '1001 Spikes', link: [{ link: 'http://adf.ly/1dmnLf', comments: 'USA' }], image: [{ src: 'app/images/cover/1.jpg' }, { src: 'app/images/cover/1.jpg' }], imageCover: "app/images/cover/1.jpg" },
+    { id: 1, title: '2013 Infected Wars', link: [{ link: 'http://adf.ly/1dj5Bo', comments: 'EUR' }, { link: 'http://adf.ly/1dmVt4', comments: 'USA' }], image: [{ src: 'app/images/cover/2.jpg' }], imageCover: "app/images/cover/2.jpg" },
+    { id: 1, title: 'A.W. Phoenix Festa', link: [{ link: 'http://adf.ly/1dfgGu', comments: 'EUR' }, { link: 'http://adf.ly/1diSsz', comments: 'JPN' }], image: [{ src: 'http://www.playincstore.com/wp-content/uploads/2016/07/awpfpsv.jpg' }, { src: 'http://cdn4.dualshockers.com/wp-content/uploads/2016/07/C¦ºQVSC¦ºQBattle.jpg' }], imageCover: "http://www.playincstore.com/wp-content/uploads/2016/07/awpfpsv.jpg" }];
+exports.t3dsGames = [{ id: 1, title: 'Warrio Land', link: [], image: [{ src: '' }], imageCover: "" }];
+exports.psp = [{ id: 1, title: 'Final Fantasy Tactics', link: [], image: [{ src: '' }, { src: '' }], imageCover: "" }];
 //# sourceMappingURL=GamesList.data.js.map
